1fc07090481650e3aef8955ffbf15d2d580e3753
V3491 (Conference ID) <v3491@cam.ac.uk>
2015-07-24 10:37:28 +0100

    hello

--- drone-backend.js
+++ drone-backend.js
@@ -2,6 +2,7 @@ var Cylon = require('cylon');
 var ws = require('nodejs-websocket');
 var bot;
 
+
 // Initialise the robot
 Cylon.robot()
     .connection("ardrone", {
@@ -16,7 +17,23 @@ Cylon.robot()
     
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    robot.drone.ftrim();
+    after(2*1000, function() {
+        robot.drone.takeoff(0.2);
+    });
+    after(2 *1000, function() {
+        robot.drone.left(0.3);
+    });
+    after(2*1000, function() {
+        robot.drone.right(0.3);
+    });
+    after(2*1000, function() {
+        robot.drone.land();
+    });
+    after(2*1000, function() {
+        robot.drone.stop();
+    });
 
 }
-
 Cylon.start();

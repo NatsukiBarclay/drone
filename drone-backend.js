17ca3ac155175e6a4e3c33c4b543bd26cd2ab2f4
V3491 (Conference ID) <v3491@cam.ac.uk>
2015-07-24 13:50:52 +0100

    circle

--- drone-backend.js
+++ drone-backend.js
@@ -29,45 +29,59 @@ function fly(robot) {
         console.log(data);
     });
     bot.drone.ftrim();
-    after(2*1000, function() {
-        bot.drone.takeoff();
-    });
-    after(3*1000, function(){
-        bot.drone.hover(0.2);
+
+    bot.drone.takeoff(function() {
+        bot.drone.animateLeds("blinkGreen", 60, 10);
+
+        bot.drone.clockwise(0.1);
+        bot.drone.front(0.1);
     });
 
-    after(4 * 1000, function() {
-        bot.drone.backFlip(5000);
-    });
 
-    after(6*1000, function() {
-        bot.drone.left(0.07);
-    });
-    after(14*1000, function(){
-       bot.drone.front (0.095)
-    });
-    after(17*1000, function() {
-        bot.drone.right(0.07);
-    });
-    after(21*1000, function() {
-        bot.drone.back(0.095);
-    });
-    after(24*1000, function() {
-        bot.drone.left(0.07);
-    });
-    after(37*1000, function() {
-        bot.drone.land();
-    });
+    //after(3*1000, function(){
+    //    bot.drone.hover(0.2);
+    //});
+    //
+    //after(7*1000, function() {
+    //    bot.drone.left(0.07);
+    //});
+    //
+    //after(14*1000, function(){
+    //    bot.drone.front (0.095)
+    //});
+    //
+    //after(18*1000, function() {
+    //    bot.drone.right(0.07);
+    //});
+    //
+    //after(22*1000, function() {
+    //    bot.drone.back(0.095);
+    //});
+    //
+    //after(29*1000, function() {
+    //    bot.drone.left(0.07);
+    //});
+    //
+    //after(40*1000, function() {
+    //    bot.drone.land();
+    //});
+
     bot.nav.on("altitudeChange", function(data) {
-        console.log("Altitude:", data);
-        // Drone is higher than 1.0 meters up
-        if (altitude > 1.0) {
+        if (data.altitude > 1.5) {
+            bot.drone.stop();
             bot.drone.land();
         }
     });
-    after(42*1000, function() {
-        bot.drone.stop();
-    });
 
 }
+
+
+// metre-based function
+function doDist(func, dist) {
+    bot.drone[func](0.25);
+    after(4000 * dist, function() {
+        bot.drone.stop();
+    });
+}
+
 Cylon.start();
